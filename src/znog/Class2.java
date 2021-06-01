2542fe9a6020f4bcb7d8a70c579b6ffc462df127
nogrif <noel.griffon@dgfip.finances.gouv.fr>
2021-06-01 16:39:48 +0200

    Modif Class2

--- src/znog/Class2.java
+++ src/znog/Class2.java
@@ -1,6 +1,57 @@
+/*
+ * Copyright (c) 2021 DGFiP - Tous droits réservés
+ */
 package znog;
 
-public class Class2
-{
+/**
+ * Class Class2 Class2.
+ */
+public class Class2{
+
+    /** co class 2. */
+    protected String coClass2;
+
+    /** li class 2. */
+    protected String liClass2;
+
+    /**
+     * Accesseur de l attribut co class 2.
+     *
+     * @return co class 2
+     */
+    public String getCoClass2()
+    {
+        return coClass2;
+    }
+
+    /**
+     * Modificateur de l attribut co class 2.
+     *
+     * @param coClass2 le nouveau co class 2
+     */
+    public void setCoClass2(String coClass2)
+    {
+        this.coClass2 = coClass2;
+    }
+
+    /**
+     * Accesseur de l attribut li class 2.
+     *
+     * @return li class 2
+     */
+    public String getLiClass2()
+    {
+        return liClass2;
+    }
+
+    /**
+     * Modificateur de l attribut li class 2.
+     *
+     * @param liClass2 le nouveau li class 2
+     */
+    public void setLiClass2(String liClass2)
+    {
+        this.liClass2 = liClass2;
+    }
 
 }
